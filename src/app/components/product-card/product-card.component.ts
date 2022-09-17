a61efcee23e02c21ac8a5b0302e25d0d17a96158
IrynaZubach <102371078+IrynaZubach@users.noreply.github.com>
2022-09-17 14:17:43 +0300

    add product card & card list components

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+export interface ProductI {
+  id: string;
+  images: string[];
+  title: string;
+  rating: number;
+  price: number;
+  category: string;
+  brand: string;
+}
 
 @Component({
   selector: 'app-product-card',
@@ -6,10 +16,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+  @Input() product: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    // console.log('ngOnInit');
+  }
+
+  public handleCardClick(): void {
+    console.log('handleCardClick', this.product);
+    const firstImage = this.product.images.shift();
+    this.product.images.push(firstImage);
   }
 
 }
